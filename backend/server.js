8b5c475751d7aabb04d59f03b16f4b38c0d51051
Efren Layan <efren.layan4@gmail.com>
2022-10-31 20:07:31 +0800

    Edited server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,13 @@ app.get("/api/meal", (req, res) => {
   res.send(data.meals);
 });
 
+// const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+// );
+
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 
